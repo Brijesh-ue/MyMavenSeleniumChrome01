67468f4b5226b011c5796f29626b3690c8fe1aec
Brijesh-ue <brijeshgowda44@gmail.com>
2026-03-24 21:17:09 +0530

    Initial Commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,83 +2,22 @@ package com.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.JavascriptExecutor;
-
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
-import org.openqa.selenium.WindowType;
-
-import java.time.Duration;
-
-public class App {
-
-    public static void main(String[] args) throws InterruptedException {
-
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        // -------- Tab 1 : SauceDemo --------
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        WebDriver driver=new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
-
+        driver.manage().window().maximize();
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
-
-        Thread.sleep(3000);
-
-        // -------- Tab 2 --------
-        driver.switchTo().newWindow(WindowType.TAB);
-        driver.get("https://automationexercise.com/");
-
-        Actions actions = new Actions(driver);
-
-        WebElement product = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(
-                        By.xpath("(//div[@class='product-image-wrapper'])[1]")
-                )
-        );
-
-        actions.moveToElement(product).perform();
-
-        WebElement addToCart = wait.until(
-                ExpectedConditions.elementToBeClickable(
-                        By.xpath("(//a[contains(text(),'Add to cart')])[1]")
-                )
-        );
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView(true);", addToCart);
-
-        addToCart.click();
-
-        WebElement continueBtn = wait.until(
-                ExpectedConditions.elementToBeClickable(
-                        By.xpath("//button[text()='Continue Shopping']")
-                )
-        );
-
-        continueBtn.click();
-
-        Thread.sleep(3000);
-
-        // -------- Tab 3 --------
-        driver.switchTo().newWindow(WindowType.TAB);
-        driver.get("https://practicetestautomation.com/practice-test-login/");
-
-        driver.findElement(By.id("username")).sendKeys("student");
-        driver.findElement(By.id("password")).sendKeys("Password123");
-        driver.findElement(By.id("submit")).click();
-
-        Thread.sleep(3000);
-
-        driver.quit();
     }
 }
+
